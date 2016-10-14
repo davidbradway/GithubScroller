eb3a2aa0cf748703a5c7e795478e3c51ee07a228
David Bradway <dpb6@duke.edu>
2016-10-14 15:03:08 -0400

    clean up

--- GithubScroller.py
+++ GithubScroller.py
@@ -6,7 +6,7 @@ from subprocess import call
 def make_n_commits(n):
     for i in range(n):
         now = dt.datetime.now().strftime("%H:%M:%S.%f")
-        call(["touch", now + '.txt'])
+        call(["touch", "." + now + '.txt'])
         call(["git", "add", "."])
         call(["git", "status"])
         call(["git", "commit", "-m", "'add file'"])
