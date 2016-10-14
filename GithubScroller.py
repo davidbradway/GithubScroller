aa11bc24413d51a74765c11ed59b0e95abb1cb3e
David Bradway <dpb6@duke.edu>
2016-10-14 15:01:29 -0400

    'add file'

--- GithubScroller.py
+++ GithubScroller.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
+import datetime as dt
 from subprocess import call
 
+
+def make_n_commits(n):
+    for i in range(n):
+        now = dt.datetime.now().strftime("%H:%M:%S.%f")
+        call(["touch", now + '.txt'])
+        call(["git", "add", "."])
+        call(["git", "status"])
+        call(["git", "commit", "-m", "'add file'"])
+        # call(["git", "push"])
+
+
 def main():
-    call(["git", "add", "."])
-    call(["git", "status"])
-    call(["git", "commit", "-m", "'add file'"])
-    #call(["git", "push"])
-    return 0
+    n = 4
+    make_n_commits(n)
+
 
 if __name__ == '__main__':
     main()
